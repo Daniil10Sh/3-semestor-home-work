a6620cc8c1e7086db42a78378dece354cc71884c
Daniil10Sh <danik2022odessa@gmail.com>
2024-06-21 15:46:37 +0300

    fixation of index , js and img

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//  const textField = document.getElementById('textField');
+//  const myButton = document.getElementById('myButton');
+//  myButton.addEventListener('click', function() {
+//      myButton.textContent = textField.value;
+//  });
+
+// const myImage = document.querySelector('.myImage');
+// myImage.src = 'danya.img.jpg';
+
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     const myLink = document.querySelector('.myLink');
+    //     myLink.href = 'https://kz.ebay.com/';
+    //     const myImage = document.querySelector('.myImage');
+    //     myImage.alt = 'Це нове авто представлене компанією Мерседес.Авто називається Mercedes-AMG ONE.Це гіперкар. Цей гіперкар знаходиться на треку';
+    // });
+
+   
+// let firstListItem = document.querySelector('#myList li');
+// firstListItem.textContent = 'Новий текст для першого елемента';
